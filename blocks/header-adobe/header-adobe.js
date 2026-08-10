273fddda4749fd01ddc17505bf754b2967d6f722
Daniel Candia <cor72081@adobe.com>
2026-08-10 13:43:24 -0600

    no message

--- blocks/header-adobe/header-adobe.js
+++ blocks/header-adobe/header-adobe.js
@@ -1,4 +1,4 @@
-/* export default function decorate(block) {
+export default function decorate(block) {
   block.innerHTML = `
     <div class="header-adobe-content">
 
@@ -39,9 +39,4 @@
   dropdownButton.addEventListener("click", () => {
     dropdownMenu.classList.toggle("is-open");
   });
-}
- */
-
-export default function decorate(block) {
-  block.textContent = "HEADER ADOBE FUNCIONANDO";
 }
